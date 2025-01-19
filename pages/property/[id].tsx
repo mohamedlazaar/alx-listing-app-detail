c43b7a1d5fed0d76545dddb1d6bfa29c73ebe5c6
mohamed lazaar <lazzarmohamed10@gmail.com>
2025-01-19 16:27:25 +0100

    fixing the name of a componenet

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,6 +1,6 @@
 import { PROPERTYLISTINGSAMPLE } from "@/constants/index";
 import { useRouter } from "next/router";
-import PropertyDetail from "../../components/property/ProperyDetail";
+import PropertyDetail from "../../components/property/PropertyDetail";
 import BookingSection from "@/components/property/BookingSection";
 import ReviewSection from "@/components/property/ReviewSection";
 
@@ -16,7 +16,7 @@ export default function PropertyPage() {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <div className="md:col-span-2">
                     <PropertyDetail property={property} />
-                    {/* <ReviewSection reviews={property.reviews} /> */}
+                    <ReviewSection reviews={property.reviews} />
                 </div>
                 <div className="md:col-span-1">
                     <BookingSection price={property.price} />
